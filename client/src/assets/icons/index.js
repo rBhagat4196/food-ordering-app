9e5ec58f77f63de4cfcf1e4f38654188ac97cdd6
rBhagat4196 <rahulbhagat13slr@gmail.com>
2024-02-17 13:01:24 +0530

    icons

---
+++ client/src/assets/icons/index.js
@@ -0,0 +1,41 @@
+import {
+  FaEnvelope,
+  FaLock,
+  FaCloudUploadAlt,
+  FaCheck,
+  FaArrowLeft,
+} from "react-icons/fa";
+
+import { FcGoogle, FcClearFilters } from "react-icons/fc";
+import { MdShoppingCart, MdLogout, MdSearch, MdDelete } from "react-icons/md";
+
+import {
+  BsToggles2,
+  BsFillBellFill,
+  BsExclamationTriangleFill,
+} from "react-icons/bs";
+
+import { IoFastFood, IoMdBasket } from "react-icons/io";
+import { BiChevronRight } from "react-icons/bi";
+import { HiCurrencyRupee } from "react-icons/hi2";
+
+export {
+    FaEnvelope,
+    FaLock,
+    FaCloudUploadAlt,
+    FaCheck,
+    FaArrowLeft,
+    FcGoogle,
+    FcClearFilters,
+    MdShoppingCart,
+    MdLogout,
+    MdSearch,
+    MdDelete,
+    BsToggles2,
+    BsFillBellFill,
+    BsExclamationTriangleFill,
+    IoFastFood,
+    IoMdBasket,
+    BiChevronRight,
+    HiCurrencyRupee
+  };
