cd24519eb3b887529c0f8306377f0b2ff13b6e3b
rBhagat4196 <rahulbhagat13slr@gmail.com>
2024-02-20 23:29:37 +0530

    bugs fixing

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,27 @@
 import React, { useState } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import { isActiveStyles, isNotActiveStyles } from "../utils/styles";
 import { motion } from "framer-motion";
 import { buttonClick, slideTop } from "../animations";
 import { MdLogout, MdShoppingCart } from "../assets/icons";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Avatar } from "../img";
-
+import {getAuth } from "firebase/auth"
+import { app } from "../config/firebase";
+import { setUserDetails } from "../redux/userSlice";
 const Header = () => {
-  const user = useSelector((state) => state.user);
+  const user = useSelector((state) => state.user.user);
   const [isMenu,setIsMenu] = useState(false);
+  const auth = getAuth(app)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const signOut = ()=>{
+    auth.signOut().then(()=>{
+      dispatch(setUserDetails(""))
+      navigate("/login",{replace : true})
+    })
+  }
   return (
     <header className="fixed backdrop-blur-md z-50 inset-x-0 top-0 flex items-center justify-between px-12 md:px-20 py-6">
       <NavLink to={"/"} className="flex items-center justify-between gap-4">
@@ -59,15 +71,15 @@ const Header = () => {
         </motion.div>
         {user ? (
           <>
-            <div className="relative cursor-pointer"
+            <div className="relative cursor-pointer -mt-2"
             onMouseEnter={() => setIsMenu(true)}
             >
               <div
-                className="w-12 h-12 rounded-full shadow-md cursor-pointer 
+                className="w-10 h-10 rounded-full shadow-md cursor-pointer 
               overflow-hidden flex items-center justify-center"
               >
                 <motion.img
-                  className="w-full h-full object-cover"
+                  className="w-full h-full object-cover "
                   src={user?.picture ? user?.picture : Avatar}
                   whileHover={{ scale: 1.2 }}
                   referrerPolicy="no-referrer"
@@ -88,18 +100,19 @@ const Header = () => {
                 </Link>
                 <Link
                   className="hover:text-red-500 text-xl text-textColor"
-                  to={"/dashboard"}
+                  to={"/profile"}
                 >
                   Profile
                 </Link>
                 <Link
                   className="hover:text-red-500 text-xl text-textColor"
-                  to={"/dashboard"}
+                  to={"/orders"}
                 >
                   Orders
                 </Link>
                 <hr/>
-                <motion.div {...buttonClick} className="group flex items-center justify-center px-3 py-2 rounded-md hover:bg-gray-200 gap-3">
+                <motion.div {...buttonClick} className="group flex items-center justify-center px-3 py-2 rounded-md hover:bg-gray-200 gap-3"
+                onClick = {signOut}>
                     <MdLogout className="text-2xl text-textColor group-hover:text-headingColor"/>
                     <p className="text-textColor text-xl group-hover:text-headingColor">
                         SignOut
--- client/src/redux/userSlice.js
+++ client/src/redux/userSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    user: {},
+    user: "",
   }
 const UserSlice = createSlice({
     name: "User",
