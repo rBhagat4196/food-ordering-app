b3beb901cdf2d028430ac45ffaa4317210dbadb2
rBhagat4196 <rahulbhagat13slr@gmail.com>
2024-02-20 20:28:18 +0530

    user profile

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,15 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 import { isActiveStyles, isNotActiveStyles } from "../utils/styles";
 import { motion } from "framer-motion";
-import { buttonClick } from "../animations";
-import { MdShoppingCart } from "../assets/icons";
+import { buttonClick, slideTop } from "../animations";
+import { MdLogout, MdShoppingCart } from "../assets/icons";
 import { useSelector } from "react-redux";
+import { Avatar } from "../img";
+
 const Header = () => {
   const user = useSelector((state) => state.user);
+  const [isMenu,setIsMenu] = useState(false);
   return (
     <header className="fixed backdrop-blur-md z-50 inset-x-0 top-0 flex items-center justify-between px-12 md:px-20 py-6">
       <NavLink to={"/"} className="flex items-center justify-between gap-4">
@@ -55,12 +58,64 @@ const Header = () => {
           </div>
         </motion.div>
         {user ? (
-          <>User</>
+          <>
+            <div className="relative cursor-pointer"
+            onMouseEnter={() => setIsMenu(true)}
+            >
+              <div
+                className="w-12 h-12 rounded-full shadow-md cursor-pointer 
+              overflow-hidden flex items-center justify-center"
+              >
+                <motion.img
+                  className="w-full h-full object-cover"
+                  src={user?.picture ? user?.picture : Avatar}
+                  whileHover={{ scale: 1.2 }}
+                  referrerPolicy="no-referrer"
+                />
+              </div>
+              {isMenu && (
+                <motion.div
+                {...slideTop}
+                className="px-6 py-4 w-48 bg-lightOverlay backdrop-blur-md 
+              rounded-md shadow-md absolute top-12 right-0 flex flex-col gap-4"
+              onMouseLeave={()=> setIsMenu(false)}
+              >
+                <Link
+                  className="hover:text-red-500 text-xl text-textColor"
+                  to={"/dashboard"}
+                >
+                  Dashboard
+                </Link>
+                <Link
+                  className="hover:text-red-500 text-xl text-textColor"
+                  to={"/dashboard"}
+                >
+                  Profile
+                </Link>
+                <Link
+                  className="hover:text-red-500 text-xl text-textColor"
+                  to={"/dashboard"}
+                >
+                  Orders
+                </Link>
+                <hr/>
+                <motion.div {...buttonClick} className="group flex items-center justify-center px-3 py-2 rounded-md hover:bg-gray-200 gap-3">
+                    <MdLogout className="text-2xl text-textColor group-hover:text-headingColor"/>
+                    <p className="text-textColor text-xl group-hover:text-headingColor">
+                        SignOut
+                    </p>
+                </motion.div>
+              </motion.div>
+              )}
+            </div>
+          </>
         ) : (
           <>
             <NavLink to={"/login"}>
-              <motion.button {...buttonClick} 
-              className="px-4 py-2 rounded-md shadow-md bg-lightOverlay border border-red-300">
+              <motion.button
+                {...buttonClick}
+                className="px-4 py-2 rounded-md shadow-md bg-lightOverlay border border-red-300"
+              >
                 Login
               </motion.button>
             </NavLink>
