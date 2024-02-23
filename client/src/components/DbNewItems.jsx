0c7f36caf683f64b3b10ea11c6926fe056b40253
rBhagat4196 <rahulbhagat13slr@gmail.com>
2024-02-23 19:37:22 +0530

    category component

--- client/src/components/DbNewItems.jsx
+++ client/src/components/DbNewItems.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React, { useState } from "react";
+import { statuses } from "../utils/styles";
 
-const DbNewItems = () => {
+export const InputValueField = ({
+  type,
+  placeHolder,
+  stateValue,
+  stateFunc,
+}) => {
   return (
-    <div>
-      New Items
+    <>
+      <input
+        type={type}
+        placeholder={placeHolder}
+        className="w-full px-4 py-3 bg-lightOverlay shadow-md outline-none rounded-md border border-gray-200 focus:border-red-400"
+        value={stateValue}
+        onChange={(e) => stateFunc(e.target.value)}
+      />
+    </>
+  );
+};
+const DbNewItems = () => {
+  const [itemName, setItemName] = useState("");
+  const [category,setCategory] = useState("")
+  return (
+    <div className="flex flex-col items-center justify-center gap-4 pt-6 w-full px-24">
+      <div className="border border-gray-300 rounded-md p-4 w-full flex flex-col items-center justify-center gap-4">
+        <InputValueField
+          type="text"
+          placeHolder="Item name here"
+          stateValue={itemName}
+          stateFunc={setItemName}
+        />
+      </div>
+      <div className="w-full flex items-center justify-around gap-3 flex-wrap">
+          {statuses &&
+            statuses?.map((data) => (
+              <p
+                key={data.id}
+                onClick={() => setCategory(data.category)}
+                className={`px-4 py-3 rounded-md text-xl text-textColor font-semibold cursor-pointer hover:shadow-md border border-gray-200 backdrop-blur-md ${
+                  data.category === category
+                    ? "bg-red-400 text-primary"
+                    : "bg-transparent"
+                }`}
+              >
+                {data.title}
+              </p>
+            ))}
+        </div>
     </div>
-  )
-}
+  );
+};
 
-export default DbNewItems
+export default DbNewItems;
